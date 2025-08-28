a435d2cd3a302cb822dcda0e7113848e9668dd4a
m0ksem <md.cheban@gmail.com>
2025-08-28 23:28:21 +0300

    Updated gitignore again

--- CodeWarsKATAs/CodeWarsKATAs/Program.cs
+++ CodeWarsKATAs/CodeWarsKATAs/Program.cs
@@ -4,3 +4,5 @@
 Console.WriteLine(MeanSquareError.Solution([1, 2, 3], [4, 5, 6]));
 Console.WriteLine(MeanSquareError.Solution([10, 20, 10, 2], [10, 25, 5, -2]));
 Console.WriteLine(MeanSquareError.Solution([-1, 0], [0, -1]));
+
+//
