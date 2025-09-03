0cda45bf8642718dbe03307bc04f1f374e7db91e
m0ksem <md.cheban@gmail.com>
2025-09-03 13:30:46 +0300

    Completed task Remove First and Last Character Part Two

--- CodeWarsKATAs/CodeWarsKATAs/Program.cs
+++ CodeWarsKATAs/CodeWarsKATAs/Program.cs
@@ -3,7 +3,17 @@ using System.Buffers.Text;
 
 
 //Console.WriteLine(NumeralSystemValidNumber.ValidateBase("1234", 8));
-Console.WriteLine(NumeralSystemValidNumber.ValidateBase("61262", 8));
+//Console.WriteLine("|" + RemoveFirstAndLastCharacter2.Array("1,2,3,4,5,6,7") + "|");
+//Console.WriteLine("|" + RemoveFirstAndLastCharacter2.Array("") + "|");
+//Console.WriteLine("|" + RemoveFirstAndLastCharacter2.Array("1") + "|");
+//Console.WriteLine("|" + RemoveFirstAndLastCharacter2.Array("1, 3") + "|");
+//Console.WriteLine("|" + RemoveFirstAndLastCharacter2.Array("1,2,3") + "|");
+//Console.WriteLine("|" + RemoveFirstAndLastCharacter2.Array("1ad,a2b") + "|");
+Console.WriteLine("|" + RemoveFirstAndLastCharacter2.Array("1,2,3,4") + "|");
+
+
+
+
 
 
 
---
+++ CodeWarsKATAs/CodeWarsKATAs/Solutions/RemoveFirstAndLastCharacter2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKATAs.Solutions
+{
+    public class RemoveFirstAndLastCharacter2
+    {
+        public static string? Array2(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Split(",").Length < 3)
+                return null;
+            
+            string result = s.Replace(" ", "");
+            string[] arr = result.Split(",");
+            string[] arr2 = arr[1..(arr.Length - 1)];
+
+
+            return String.Join(" ", arr2);
+
+        }
+
+        public static string? Array(string s) => (string.IsNullOrEmpty(s) || s.Split(",").Length < 3) ? null 
+            : String.Join(" ", s.Replace(" ", "").Split(",")[1..^1]);
+    }
+}
