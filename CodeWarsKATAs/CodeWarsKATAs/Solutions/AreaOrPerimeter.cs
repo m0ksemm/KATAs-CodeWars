1ae2147ad63d397c60a6af64281a215f079f3435
m0ksem <md.cheban@gmail.com>
2025-09-04 18:36:37 +0300

    Completed task AreaOrPerimeter.cs

---
+++ CodeWarsKATAs/CodeWarsKATAs/Solutions/AreaOrPerimeter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKATAs.Solutions
+{
+    public class AreaOrPerimeter
+    {
+        public static int MathCheck(int l, int w) => l == w ? l * w : (l + w) * 2;
+    }
+}
---
+++ CodeWarsKATAs/CodeWarsKATAs/Solutions/Thinkful-NumberDrills.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKATAs.Solutions
+{
+    internal class Thinkful_NumberDrills
+    {
+    }
+}
