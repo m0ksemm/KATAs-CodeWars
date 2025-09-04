0afa948c84219d92ea927028856a798b01ba2661
m0ksem <md.cheban@gmail.com>
2025-09-05 01:29:06 +0300

    Completed Print count and numbers Kata task.

--- CodeWarsKATAs/CodeWarsKATAs/Program.cs
+++ CodeWarsKATAs/CodeWarsKATAs/Program.cs
@@ -8,9 +8,16 @@ using System.Buffers.Text;
 //Console.WriteLine("|" + RemoveFirstAndLastCharacter2.Array("1") + "|");
 //Console.WriteLine("|" + RemoveFirstAndLastCharacter2.Array("1, 3") + "|");
 //Console.WriteLine("|" + RemoveFirstAndLastCharacter2.Array("1,2,3") + "|");
-//Console.WriteLine("|" + RemoveFirstAndLastCharacter2.Array("1ad,a2b") + "|");
-Console.WriteLine(TheRoad_KillDetective.RoadKill("=====r=rrr=rra=====eee======bb====b======="));
-
+Console.WriteLine(PrintCountAndNumbers.CountMe("1123"));
+Console.WriteLine(PrintCountAndNumbers.CountMe("1"));
+Console.WriteLine(PrintCountAndNumbers.CountMe("a"));
+Console.WriteLine(PrintCountAndNumbers.CountMe("11"));
+Console.WriteLine(PrintCountAndNumbers.CountMe("a123"));
+Console.WriteLine(PrintCountAndNumbers.CountMe("21"));
+Console.WriteLine(PrintCountAndNumbers.CountMe("1211"));
+Console.WriteLine(PrintCountAndNumbers.CountMe("12322212223443"));
+Console.WriteLine(PrintCountAndNumbers.CountMe(""));
+Console.WriteLine(PrintCountAndNumbers.CountMe("123a"));
 
 
 
---
+++ CodeWarsKATAs/CodeWarsKATAs/Solutions/PrintCountAndNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeWarsKATAs.Solutions
+{
+    public class PrintCountAndNumbers
+    {
+        public static string CountMe(string data)
+        {
+            if (string.IsNullOrEmpty(data)) 
+                return "";
+
+            foreach (var item in data) 
+            {
+                if (char.IsLetter(item))
+                    return "";
+            }
+
+            string result = "";
+            int count = 0;
+            for (int i = 0; i < data.Length; i++) 
+            {
+                count = 0;
+                int j = i;
+                do
+                {
+                    count++;
+                    j++;
+                }
+                while (j < data.Length && data[i] == data[j]);
+                result += count.ToString() + data[i];
+                i = j - 1;
+            }
+
+            return result;
+        }
+
+        public static string CountMe2(string data)
+        {
+            return data.Any(char.IsLetter) ? "" : Regex.Replace(data, @"(.)\1*", m => $"{m.Length}{m.Value[0]}");
+        }
+    }
+}
