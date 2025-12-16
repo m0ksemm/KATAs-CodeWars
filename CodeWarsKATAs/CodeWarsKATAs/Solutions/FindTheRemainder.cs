d93f255d39ecfb3debaf80d6135c9a6b83770a9c
m0ksem <md.cheban@gmail.com>
2025-12-16 15:31:05 +0200

    FindTheRemainder

---
+++ CodeWarsKATAs/CodeWarsKATAs/Solutions/FindTheRemainder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKATAs.Solutions
+{
+    public class FindTheRemainder
+    {
+        public static int Remainder(int a, int b)  => a > b ? b == 0 ? throw new DivideByZeroException() : a % b : a == 0 ? throw new DivideByZeroException() : b % a;
+        //{
+        //    if (a > b)
+        //        if (b == 0)
+        //            throw new DivideByZeroException();
+        //        else return a % b;
+        //    else if (a == 0)
+        //        throw new DivideByZeroException();
+        //    else return b % a;
+        //}
+
+
+        //    public static int Remainder(int a, int b) =>
+        //a > b ? a % b : b % a;
+        //public static int Remainder(int a, int b) => Math.Max(a, b) % Math.Min(a, b);
+    }
+}
---
+++ CodeWarsKATAs/CodeWarsKATAs/Solutions/Plus1Array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKATAs.Solutions
+{
+    public class Plus1Array
+    {
+        public static int[] UpArray(int[] num)
+        {
+            string s = String.Join("", num);
+            int number = int.Parse(s) + 1;
+            string s2 = number.ToString();
+            int[] array = new int[s2.Length];
+            foreach (string st in s2)
+            {
+
+            }
+            return array;
+        }
+    }
+}
